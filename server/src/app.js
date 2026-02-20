3b8db3b7bd0bd7e2256fa90f4a5d3edd7384ff9b
soumishjain <jainmashu13@gmail.com>
2026-02-20 19:06:00 +0530

    temp console

--- server/src/app.js
+++ server/src/app.js
@@ -10,7 +10,8 @@ const app = express()
 app.use(express.json())
 app.use(clerkMiddleware())
 app.use('/api/inngest', serve({client : inngest,functions}))
-
+console.log("ENV EVENT:", process.env.INGEST_EVENT_KEY);
+console.log("ENV SIGNING:", process.env.INGEST_SIGNING_KEY);
 
 connectToDb()
 
