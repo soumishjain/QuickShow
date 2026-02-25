b1f5a0cedfb728cfba872106c9128afffe03e17f
soumishjain <jainmashu13@gmail.com>
2026-02-26 01:07:17 +0530

    ..

--- server/src/app.js
+++ server/src/app.js
@@ -22,9 +22,9 @@ connectToDb()
 
 app.use('/api/stripe' , express.json(), stripeWebhooks)
 
+app.use(express.json())
 
 app.use('/api/inngest', serve({client : inngest,functions}))
-app.use(express.json())
 app.use(clerkMiddleware())
 app.use('/api/show',showRouter)
 
